01aaa7490d0955b0268a575d822c9e58de2d1420
Rakesh_Navale <rackeshnavale87@users.noreply.github.com>
2016-04-14 22:52:01 -0700

    Update BST_All_Operations_Functions.java

--- BST_All_Operations_Functions.java
+++ BST_All_Operations_Functions.java
@@ -18,6 +18,43 @@ class Node
 	}
 }
 
+// Binary tree common ancestor *******************************
+static boolean v1 = false, v2 = false;
+ Node findLCA(int n1, int n2) {
+     v1 = false;
+     v2 = false;
+     Node lca = findLCAUtil(root, n1, n2);
+     if (v1 && v2)
+        return lca;
+     else
+     	return null;
+ }
+ Node findLCAUtil(Node node, int n1, int n2) {
+	 if(node == null)
+		 return null;
+	 if(node.data == n1 && node.data == n2) {
+		 v1=true;
+		 v2 = true;
+		 return node;
+	 }
+	 if(node.data == n1) {
+		 v1 = true;
+		 return node;
+	 } 
+	 if(node.data==n2) {
+		 v2 = true;
+		 return node;
+	 }
+	 Node left = findLCAUtil(node.left, n1,n2);
+	 Node right = findLCAUtil(node.right,n1,n2);
+	 
+	 if(left!=null && right !=null)
+		 return node;
+	 return left!=null ? left : right;
+ }
+ // Finds lca of n1 and n2 under the subtree rooted with 'node'
+
+// Binary tree common ancestor *******************************
 
 public class BST_All_Operations_Functions 
 {
